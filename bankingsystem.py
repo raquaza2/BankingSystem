b8f0f452da12cc6d50eb13b79a0d7f77f4a9fe6a
raquaza2 <57903323+raquaza2@users.noreply.github.com>
2024-10-12 10:55:14 +0800

    Update bankingsystem.py

--- bankingsystem.py
+++ bankingsystem.py
@@ -1,27 +1,50 @@
-def show_balance():
-    pass
+def show_balance():   
+    print(f"Your balance: RM{balance:.2f}")
+  
 
 def deposit():
-    pass
+    dep = float(input("Please enter you amount:RM"))
+    
+    if dep < 0:
+        print("Invalid amount")
+        return 0
+    else: 
+        return dep
 
 def withdraw():
-    pass
+    wd = float(input("Please enter you amount:RM"))
+    
+    if wd <= 0:
+        print("Invalid amount")
+        return 0
+    elif wd > balance: 
+        print("Insufficient balance!")
+        return 0
+    else:
+        return wd
 
- 
+dep = 0
 balance = 0
 is_running = True
 
 while is_running:
+    print("------------------------")
     print("Banking Program")
+    print("------------------------")
     choice = input("Press 'd' to deposit, 'w' to withdraw, 'b' to show balace, 'q' to quit:")
     
     if choice == 'd':
-        show_balance()
+        balance += deposit()
+        
     elif choice == 'w':
-        withdraw()
+        balance -= withdraw()
+        
     elif choice == 'b':
         show_balance()
+        
     elif choice == 'q':
         is_running = False
     else:
         print("Invalid choice. Please try again.")
+           
+print("Thank you!")
