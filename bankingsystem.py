e3585d91b68e2cb5df40e3c54303ba58003f13cf
raquaza2 <57903323+raquaza2@users.noreply.github.com>
2024-10-12 10:18:48 +0800

    Selection
    Selection choice using 'IF-ELSE'

--- bankingsystem.py
+++ bankingsystem.py
@@ -8,3 +8,20 @@ def withdraw():
     pass
 
  
+balance = 0
+is_running = True
+
+while is_running:
+    print("Banking Program")
+    choice = input("Press 'd' to deposit, 'w' to withdraw, 'b' to show balace, 'q' to quit:")
+    
+    if choice == 'd':
+        show_balance()
+    elif choice == 'w':
+        withdraw()
+    elif choice == 'b':
+        show_balance()
+    elif choice == 'q':
+        is_running = False
+    else:
+        print("Invalid choice. Please try again.")
